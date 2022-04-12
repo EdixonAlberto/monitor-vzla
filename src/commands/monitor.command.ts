9cad62977a478484bd538a8a5f69773a980dcd20
Edixon <edixonalbertto@gmail.com>
2022-04-12 02:36:33 -0400

    move logic monitor.command -> ready.event
    - installs libraries
    - update library concord
    - create folder /events
    - install prettier and add script format

--- src/commands/monitor.command.ts
+++ src/commands/monitor.command.ts
@@ -1,55 +1,5 @@
 import { TCommand } from '@edixon/concord'
-import { MonitorService } from '../Monitor.service'
-import { IntervalService } from '../Interval.service'
 
 export const monitor: TCommand = async ({ response }) => {
-  const monitorService = new MonitorService()
-  const interval = new IntervalService(['09:30', '13:30'])
-
-  try {
-    await response.general('>> START <<')
-    interval.start(notifyPriceUpdated(monitorService, response))
-  } catch (error) {
-    console.error(error)
-    response.general('>> ERROR <<')
-  }
-}
-
-function notifyPriceUpdated(monitor, response) {
-  return async () => {
-    const monitorDollar = await monitor.getPriceMonitorDollar()
-    if (!monitorDollar) return
-
-    const { url, avatar, account, price } = monitorDollar
-
-    response.embeded({
-      header: {
-        text: 'Monitor Dolar',
-        url: `https://www.instagram.com/${account}`
-      },
-      imageHeader: avatar,
-      body: [
-        {
-          title: 'Precio',
-          content: price.amount.toString() + 'Bs',
-          fieldType: 'column'
-        },
-        {
-          title: 'Tendencia',
-          content: price.chg.trend.icon + price.chg.percentage,
-          fieldType: 'column'
-        },
-        {
-          title: 'Hora',
-          content: price.time,
-          fieldType: 'column'
-        },
-        {
-          title: 'Fuente',
-          content: `[@${account}](${url})`
-        }
-      ],
-      footer: `Fecha: ${price.date}`
-    })
-  }
+  console.log('Command monitor')
 }
---
+++ src/events/ready.event.ts
@@ -0,0 +1,57 @@
+import { TEvent } from '@edixon/concord'
+import { BotResponse } from '@edixon/concord/dist/core/BotResponse'
+import { MonitorService } from '../Monitor.service'
+import { IntervalService } from '../Interval.service'
+
+export const ready: TEvent = async ({ channels }) => {
+  const monitorService = new MonitorService()
+  const interval = new IntervalService(['09:30', '13:30'])
+  const channel = channels.get('827708188907929673')
+
+  try {
+    if (channel) {
+      interval.start(notifyPriceUpdated(monitorService, channel))
+    } else throw new Error('Channel "827708188907929673" not found')
+  } catch (error) {
+    console.error((error as Error).message)
+  }
+}
+
+function notifyPriceUpdated(monitor: MonitorService, channel: BotResponse) {
+  return async () => {
+    const monitorDollar = await monitor.getPriceMonitorDollar()
+    if (!monitorDollar) return
+
+    const { url, avatar, account, price } = monitorDollar
+
+    channel.embeded({
+      header: {
+        text: 'Monitor Dolar',
+        url: `https://www.instagram.com/${account}`
+      },
+      imageHeader: avatar,
+      body: [
+        {
+          title: 'Precio',
+          content: price.amount.toString() + 'Bs',
+          fieldType: 'column'
+        },
+        {
+          title: 'Tendencia',
+          content: price.chg.trend.icon + price.chg.percentage,
+          fieldType: 'column'
+        },
+        {
+          title: 'Hora',
+          content: price.time,
+          fieldType: 'column'
+        },
+        {
+          title: 'Fuente',
+          content: `[@${account}](${url})`
+        }
+      ],
+      footer: `Fecha: ${price.date}`
+    })
+  }
+}
